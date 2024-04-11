34cc4a9c5badb88928b183e10c5ac17aff05fa2f
Opilane TTHK <opilane@tthk.ee>
2024-04-11 09:44:29 +0300

    Tabel tootaja

--- lilledeMyyk.sql
+++ lilledeMyyk.sql
@@ -2,3 +2,13 @@ CREATE DATABASE lilledeMyykKauler;
 USE lilledeMyykKauler;
 
 --tabel tootaja
+CREATE TABLE tootaja(
+tootajaID INT PRIMARY KEY identity(1,1),
+eesnimi VARCHAR(20),
+perekonnanimi VARCHAR(20),
+isikukood VARCHAR(11) UNIQUE
+)
+SELECT * FROM tootaja;
+
+INSERT INTO tootaja(eesnimi,perekonnanimi,isikukood)
+VALUES ('Donat','Kauler','50512241441');
