45c93cd73f87cfc8b043c0af41850ba2a85c9186
BASEWOOD <127052755+donatkauler22@users.noreply.github.com>
2024-04-30 15:20:28 +0300

    Add files via upload

---
+++ loojaNimiSQLTITkasutajad.sql
@@ -0,0 +1,8 @@
+Use TITkasutajad;
+ALTER TABLE opilane add perenimi varchar(25)
+select * from opilane;
+CREATE TABLE opetaja(
+ID int,
+Nimi varchar(25));
+INSERT INTO opetaja(Nimi)
+VALUES ('Test');
---
+++ opilaneDonatSQLTITkasutajad.sql
@@ -0,0 +1,8 @@
+SELECT * FROM opilane;
+INSERT INTO opilane (id, opilane)
+VALUES (2, 'Donat')
+
+DELETE FROM opilane;
+CREATE TABLE test (id int)
+Update opilane set id=3;
+
