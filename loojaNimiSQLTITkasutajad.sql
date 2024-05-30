98eabf610748d7511616a06b390f6cd81e8133b9
BASEWOOD <127052755+donatkauler22@users.noreply.github.com>
2024-05-30 11:12:50 +0300

    Delete loojaNimiSQLTITkasutajad.sql

--- loojaNimiSQLTITkasutajad.sql
+++
@@ -1,8 +0,0 @@
-Use TITkasutajad;
-ALTER TABLE opilane add perenimi varchar(25)
-select * from opilane;
-CREATE TABLE opetaja(
-ID int,
-Nimi varchar(25));
-INSERT INTO opetaja(Nimi)
-VALUES ('Test');
